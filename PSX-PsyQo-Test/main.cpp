17eb3772c8ef876fa4324659ec157f6d674baf45
Jason Knoll <jasonknoll98@aol.com>
2024-10-14 11:09:31 -0400

    added primitive example

--- PSX-PsyQo-Test/main.cpp
+++ PSX-PsyQo-Test/main.cpp
@@ -6,37 +6,21 @@
 #include "third_party/nugget/psyqo/gpu.hh"
 #include "third_party/nugget/psyqo/scene.hh"
 
-namespace {
 
-// A PSYQo software needs to declare one \`Application\` object.
-// This is the one we're going to do for our hello world.
-class Hello final : public psyqo::Application {
-
+class App : public psyqo::Application {
     void prepare() override;
     void createScene() override;
-
-  public:
-    psyqo::Font<> m_font;
 };
 
-// And we need at least one scene to be created.
-// This is the one we're going to do for our hello world.
-class HelloScene final : public psyqo::Scene {
+class HelloAppScene : public psyqo::Scene {
     void frame() override;
-
-    // We'll have some simple animation going on, so we
-    // need to keep track of our state here.
-    uint8_t m_anim = 0;
-    bool m_direction = true;
 };
 
-// We're instantiating the two objects above right now.
-Hello hello;
-HelloScene helloScene;
+HelloAppScene rootScene;
 
-}  // namespace
 
-void Hello::prepare() {
+// I don't know enough to change this lol
+void App::prepare() {
     psyqo::GPU::Configuration config;
     config.set(psyqo::GPU::Resolution::W320)
         .set(psyqo::GPU::VideoMode::AUTO)
@@ -45,28 +29,16 @@ void Hello::prepare() {
     gpu().initialize(config);
 }
 
-void Hello::createScene() {
-    m_font.uploadSystemFont(gpu());
-    pushScene(&helloScene);
+void App::createScene() {
+    pushScene(&rootScene);
 }
 
-void HelloScene::frame() {
-    if (m_anim == 0) {
-        m_direction = true;
-    } else if (m_anim == 255) {
-        m_direction = false;
-    }
-    psyqo::Color bg{{.r = 0, .g = 64, .b = 91}};
-    bg.r = m_anim;
-    hello.gpu().clear(bg);
-    if (m_direction) {
-        m_anim++;
-    } else {
-        m_anim--;
-    }
-
-    psyqo::Color c = {{.r = 255, .g = 255, .b = uint8_t(255 - m_anim)}};
-    hello.m_font.print(hello.gpu(), "Hello World!", {{.x = 16, .y = 32}}, c);
+void HelloAppScene::frame() {
+    psyqo::Color c = {{.r = 0, .g = 0, .b = uint8_t(gpu().getFrameCount() % 255)}};
+    gpu().clear(c);
 }
 
-int main() { return hello.run(); }
+int main() { 
+    App app;   
+    return app.run(); 
+}
